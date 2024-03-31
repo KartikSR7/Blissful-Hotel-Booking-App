a5b7c14107beb6fe60ad081c55be212618b8b8f6
Kartik <98168089+KartikSR7@users.noreply.github.com>
2024-03-31 02:48:29 +0100

    Sort Function Filter

--- frontend/src/components/PriceFilter.tsx
+++ frontend/src/components/PriceFilter.tsx
@@ -7,7 +7,9 @@ const PriceFilter = ({ selectedPrice, onChange}: Props) => {
     return (
     <div>
         <h4 className="text-md font-semibold mb-2"></h4>
-        <select value={selectedPrice}
+        <select 
+         className="p-2 border rounded-md w-full"
+         value={selectedPrice}
          onChange={(event)=> 
          onChange(
             event.target.value ? parseInt(event.target.value): undefined
